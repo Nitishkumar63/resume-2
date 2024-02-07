2f52aef67242604c39afa21afc64987357e90194
nitishkumar <nitishkumar225532gmail.com>
2024-02-07 10:27:37 +0530

    created

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+	experimental: {
+		serverActions: true,
+	},
+	async headers() {
+		return [
+			{
+				source: "/api/mail",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "false" },
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "https://www.google.com/",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,DELETE,PATCH,POST,PUT",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+					},
+				],
+			},
+		];
+	},
+};
