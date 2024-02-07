bdb30169ce22f68cee1f641be44a5399c4261be8
nitishkumar <nitishkumar225532gmail.com>
2024-02-07 10:32:55 +0530

    created

--- next.config.js
+++ next.config.js
@@ -1,30 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
-	experimental: {
-		serverActions: true,
-	},
+const nextConfig = {
+	experimental: {},
 	async headers() {
-		return [
+	  return [
+		{
+		  source: "/api/:path*",
+		  headers: [
+			{ key: "Access-Control-Allow-Credentials", value: "true" },
+			{ key: "Access-Control-Allow-Origin", value: "https://master--nitishkum.netlify.app" },
 			{
-				source: "/api/mail",
-				headers: [
-					{ key: "Access-Control-Allow-Credentials", value: "false" },
-					{
-						key: "Access-Control-Allow-Origin",
-						value: "https://www.google.com/",
-					},
-					{
-						key: "Access-Control-Allow-Methods",
-						value: "GET,DELETE,PATCH,POST,PUT",
-					},
-					{
-						key: "Access-Control-Allow-Headers",
-						value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
-					},
-				],
+			  key: "Access-Control-Allow-Methods",
+			  value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
 			},
-		];
+			{
+			  key: "Access-Control-Allow-Headers",
+			  value:
+				"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+			},
+		  ],
+		},
+	  ];
 	},
-};
+  };
+  
+  module.exports = nextConfig;
+  
